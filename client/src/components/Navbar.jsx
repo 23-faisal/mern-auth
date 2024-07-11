6a9e972c4ca1c7a9e5d27abc5f94cdc3800b5ba0
23-faisal <a.faisal23r@gmail.com>
2024-07-11 19:10:36 +0600

    create header component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="w-full bg-slate-200 py-4">
+      <nav className="max-w-6xl mx-auto flex items-center justify-between">
+        <div>
+          <h1 className="font-bold text-xl text-slate-950">MERN Auth</h1>
+        </div>
+        <div className="flex items-center gap-4 font-semibold text-slate-800 ">
+          <Link to="/">Home </Link>
+          <Link to="/about">About </Link>
+          <Link to="/sign-in">Sign In </Link>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
